6e7b379a26a74fe66b56e8caf78a184bd4278555
TheGuyDangerous <sannidhyadubey@gmail.com>
2024-10-08 01:05:31 +0530

    Fixed all the light mode issues

--- lib/widgets/library/empty_state.dart
+++ lib/widgets/library/empty_state.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import '../../theme_provider.dart';
 
 class EmptyState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Iconsax.clock, size: 64, color: Colors.grey),
+          Icon(Iconsax.clock,
+              size: 64,
+              color: themeProvider.isDarkMode ? Colors.grey : Colors.grey[600]),
           SizedBox(height: 16),
           Text(
             'Your search history will appear here',
-            style: TextStyle(color: Colors.white, fontSize: 16),
+            style: TextStyle(
+                color: themeProvider.isDarkMode ? Colors.white : Colors.black,
+                fontSize: 16),
           ),
         ],
       ),
--- lib/widgets/library/incognito_message.dart
+++ lib/widgets/library/incognito_message.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import '../../theme_provider.dart';
 
 class IncognitoMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Iconsax.shield_tick, size: 64, color: Colors.grey),
+          Icon(Iconsax.shield_tick,
+              size: 64,
+              color: themeProvider.isDarkMode ? Colors.grey : Colors.grey[600]),
           SizedBox(height: 16),
           Text(
             'Incognito Mode Active',
             style: TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                color: themeProvider.isDarkMode ? Colors.white : Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Text(
             'Your search history is not being saved',
-            style: TextStyle(color: Colors.white70, fontSize: 14),
+            style: TextStyle(
+                color:
+                    themeProvider.isDarkMode ? Colors.white70 : Colors.black54,
+                fontSize: 14),
           ),
         ],
       ),
